b6ce46705baf2b51dca729d4accf08d2e8c9b85f
Programming With Claudio <79118475+programmingwithclaudio@users.noreply.github.com>
2025-11-13 01:00:51 -0500

    Remove code for reading and printing parquet data

--- ia/test.py
+++ ia/test.py
@@ -1,13 +1,6 @@
 import polars as pl
 
-# Ruta del archivo parquet
 data1_path = r"C:\Users\oak\ti\api-fastapi-login-example\datasets\processed_docs\chunks_procesados.parquet"
-
-# Leer el archivo parquet
 data1 = pl.read_parquet(data1_path)
-
-# Imprimir las primeras líneas
 print(data1.head())
-
-# Imprimir el número de filas
 print("Número total de filas:", data1.height)
